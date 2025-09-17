54f6e73653c125d31dfabeaf6b716cd3e542ace9
rbinar <rbinar@gmail.com>
2025-09-17 13:56:25 +0300

    Initial Commit

---
+++ MenuBarColorPicker/MenuBarColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarColorPickerApp.swift
+//  MenuBarColorPicker
+//
+//  Created by Remzi Binar on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
